bc183001280f8a250b69ed6f8872e236364a82e7
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-03-01 12:02:59 -0400

    Add user reference to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_153604) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_160028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_153604) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_comments_on_author_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_153604) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users", column: "author_id"
 end
