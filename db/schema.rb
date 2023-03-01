7e041dccf7e748ba7ba1886a62d0edecd4b9f957
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-03-01 12:21:26 -0400

    Add post reference to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_160028) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_162013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_160028) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id"
+    t.bigint "post_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -45,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_01_160028) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
 end
