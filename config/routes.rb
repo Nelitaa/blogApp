ea5c5bdd25612ce8aaf3f31f8ce77050f1f8317a
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-03-06 16:47:25 -0400

    Create posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root 'users#index'
+
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
