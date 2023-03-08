9c5e260c72f2825bff7ac66fb31143977e3b87fe
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-03-08 13:55:18 -0400

    Fix linter errors

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -3,34 +3,34 @@ require 'rails_helper'
 RSpec.describe 'PostsController', type: :request do
   describe 'POST index' do
     it 'returns response status success' do
-      get posts_path 
+      get user_posts_path(user_id: 1)
       expect(response).to have_http_status(200)
     end
 
     it 'renders post template' do
-      get posts_path 
-      expect(response).to render_template('post')
+      get user_posts_path(user_id: 1)
+      expect(response).to render_template('index')
     end
 
     it 'response body includes correct placeholder text' do
-      get posts_path 
+      get user_posts_path(user_id: 1)
       expect(response.body).to include('Posts')
     end
   end
 
   describe 'POST show' do
     it 'returns response status success' do
-      get user_post_path (user_id: 1, id: 1)
+      get user_post_path(user_id: 1, id: 1)
       expect(response).to have_http_status(200)
     end
 
     it 'renders show template' do
-      get user_post_path (user_id: 1, id: 1)
+      get user_post_path(user_id: 1, id: 1)
       expect(response).to render_template('show')
     end
 
     it 'response body includes correct placeholder text' do
-      get user_post_path (user_id: 1, id: 1)
+      get user_post_path(user_id: 1, id: 1)
       expect(response.body).to include('Post')
     end
   end
--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe 'UsersController', type: :request do
   describe 'GET index' do
     it 'returns response status success' do
-      get users_path 
+      get users_path
       expect(response).to have_http_status(200)
     end
 
     it 'renders index template' do
-      get users_path 
+      get users_path
       expect(response).to render_template('index')
     end
 
     it 'response body includes correct placeholder text' do
-      get users_path 
+      get users_path
       expect(response.body).to include('Users')
     end
   end
