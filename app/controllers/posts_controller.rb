4a8e5a27b6fe82e73156e7f75ee92c29ffcfe433
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-03-10 15:23:27 -0400

    Implement create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,4 +13,23 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.new
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.new(post_params)
+    @post.author = current_user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    if @post.save
+      flash[:success] = "Post created!"
+      redirect_to user_posts_path(current_user)
+    else
+      flash[:error] = "Post not created!"
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
