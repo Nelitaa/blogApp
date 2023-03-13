70fc77cd72c032a367693f31395aa87e8d934ed0
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-03-13 16:10:56 -0400

    m

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ApplicationRecord
   end
 
   def recent_comments
-    comments.order(created_at: :desc).limit(5)
+    comments.includes(:author).order(created_at: :desc).limit(5)
   end
 
   validates :title, presence: true, length: { maximum: 250 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
 
   def recent_posts
-    posts.order(created_at: :desc).limit(3)
+    posts.includes(:author).order(created_at: :desc).limit(3)
   end
 
   validates :name, presence: true
