4e1d09d29542275dcab00e8d5c4c73cf2072c07c
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-15 01:08:15 +0400

    Correct user_spec errors

--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -3,21 +3,22 @@ require 'rails_helper'
 RSpec.describe 'User', type: :feature do
   describe 'User index page' do
     before(:each) do
-      @user = User.create(name: 'John',
+      @user = User.create(name: 'Nela',
                           photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2432&q=80',
                           bio: 'I am a user')
       visit users_path
     end
 
-    it 'shows the username of all other users' do
-      User.all.each do |_user|
-        expect(page).to have_content('John')
+    it 'Redirected to the user show page when clicking on the username' do
+      User.all.each do |user|
+        click_link('Nela')
+        expect(page).to have_current_path(user_path(user.id))
       end
     end
 
-    it 'shows the profile picture for each user' do
+    it 'shows the username of all other users' do
       User.all.each do |_user|
-        expect(page).to have_css('img')
+        expect(page).to have_content('Nela')
       end
     end
 
@@ -27,10 +28,9 @@ RSpec.describe 'User', type: :feature do
       end
     end
 
-    it 'Redirected to the user show page when clicking on the username' do
-      User.all.each do |user|
-        click_link('John')
-        expect(page).to have_current_path(user_path(user.id))
+    it 'shows the profile picture for each user' do
+      User.all.each do |_user|
+        expect(page).to have_css('img')
       end
     end
   end
