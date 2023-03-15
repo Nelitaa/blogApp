16d183727083971031552357acac2d22ee06ec87
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-03-15 19:07:57 -0400

    Add devise to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 'users#index'
-
+  devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
   end
+
+  root 'users#index'
 end
