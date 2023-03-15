42ba5a08dfa6ed68ec8fb0c5922edeb18c5dddf6
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-16 03:14:07 +0400

    Update User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
-  
   validates :name, presence: true
   validates :email, presence: true
   validates :encrypted_password, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
