3e33f748d54f34632a3c29307f6a65617d73ab7b
emmanuel mhango <emmanuellmhango@gmail.com>
2023-03-16 03:13:29 +0400

    Adding name field in the views/devise/new.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
@@ -10,7 +15,4 @@ class User < ApplicationRecord
   def recent_posts
     posts.includes(:author).order(created_at: :desc).limit(3)
   end
-
-  validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
