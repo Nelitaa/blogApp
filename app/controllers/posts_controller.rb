176360ebda30209691eeeb6f40cbe3279cb9fed4
Marianela Muñoz Gutierrez <marianelamunozgutierrez@gmail.com>
2023-03-17 00:00:05 -0400

    Fix linter error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
-  
+
   def index
     @user = User.includes(:posts).find(params[:user_id])
     @posts = @user.posts.includes(:comments)
